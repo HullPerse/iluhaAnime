7934e0526ca992c06e1633c70e8798f61442f1bc
Alexandr Kochegarov <35864804+HullPerse@users.noreply.github.com>
2026-05-10 04:47:12 +0300

    ADDED: modal component

---
+++ src/components/shared/modal.component.tsx
@@ -0,0 +1,23 @@
+import { ModalWindow } from "@/types";
+import { Button } from "../ui/button.component";
+import { X } from "lucide-react";
+
+function Modal({ header, onClose, children }: ModalWindow) {
+  return (
+    <main className="flex flex-col absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 inset-0 z-50 items-center w-150 h-fit min-h-42 max-h-[80vh] windows95-active-border bg-primary shadow-lg">
+      <section className="flex flex-row items-center justify-between bg-secondary w-full p-1">
+        <span className="text-white windows95-text font-bold line-clamp-1">
+          {header}
+        </span>
+        <Button size="icon" className="size-4" onClick={onClose}>
+          <X />
+        </Button>
+      </section>
+      <section className="flex flex-col gap-1 p-2 overflow-hidden flex-1 w-full">
+        {children}
+      </section>
+    </main>
+  );
+}
+
+export default Modal;
